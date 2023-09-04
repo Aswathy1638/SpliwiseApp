2f15e8b05d06357a4c913d9f4bba28a69d925999
Aswathy1638 <aswathy1638@gmail.com>
2023-09-04 16:29:04 +0530

    Updated Balance logic

--- SpliwiseApp/Models/Expense.cs
+++ SpliwiseApp/Models/Expense.cs
@@ -15,4 +15,16 @@
         public ICollection<Transaction> Transactions { get; set; }
 
     }
+
+    public class CreateExpense 
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int GroupId { get; set; }
+        public int UserId { get; set; }
+        public int paiduser_id { get; set; }
+        public decimal amount { get; set; }
+        public decimal shareAmount { get; set; }
+
+    }
 }
